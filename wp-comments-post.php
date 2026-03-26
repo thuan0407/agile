3f763e788ddc8d6cd9ee77aadbf2973f7ae1cf5f
Mầu Văn Hiếu <hieuv8844@gmail.com>
2026-03-27 01:36:39 +0700

    test

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -10,6 +10,7 @@ if ( 'POST' !== $_SERVER['REQUEST_METHOD'] ) {
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0', 'HTTP/3' ), true ) ) {
 		$protocol = 'HTTP/1.0';
 	}
+	sfad;
 
 	header( 'Allow: POST' );
 	header( "$protocol 405 Method Not Allowed" );
