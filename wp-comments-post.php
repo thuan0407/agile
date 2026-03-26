5c3cb62bdb57bd93ca4eb12a954b29280749a566
Tung Anh <nguyentunganh111222333@gmail.com>
2026-03-27 01:32:12 +0700

    confilcs

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -5,17 +5,7 @@
  * @package WordPress
  */
 
-if ( 'POST' !== $_SERVER['REQUEST_METHOD'] ) {
-	$protocol = $_SERVER['SERVER_PROTOCOL'];
-	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0', 'HTTP/3' ), true ) ) {
-		$protocol = 'HTTP/1.0';
-	}
 
-	header( 'Allow: POST' );
-	header( "$protocol 405 Method Not Allowed" );
-	header( 'Content-Type: text/plain' );
-	exit;
-}
 
 /** Sets up the WordPress Environment. */
 require __DIR__ . '/wp-load.php';
